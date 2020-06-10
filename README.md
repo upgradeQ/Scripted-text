ebc52bd84a40c8fb38af51ac99bf93c75c562938
FooBar <18361586+HQupgradeHQ@users.noreply.github.com>
2020-06-10 07:48:56 +0300

    removed random chars

--- README.md
+++ README.md
@@ -4,18 +4,17 @@ Tested in python3.6 OBS Studio version > 25.0.0
 # Usage
 Create text source.
 _Optionally create media source_  
-Open `Tools>Scripts` ,select this script , set settings for it, preview it if need, set hotkey in `File>Settings`
+- Open `Tools>Scripts`
+- select this script 
+- set settings for it, change duration and refresh rate
+- preview it if need,
+- set hotkey in `File>Settings`
 # Current implemented text effects
 - static 
 > just show text
-- rainbow
-![preview](https://i.imgur.com/GmhEDv4.gif)
 > cycle threw colors 
-- blinking
-![preview](https://i.imgur.com/2M2wDUD.gif) 
+![preview](https://i.imgur.com/GmhEDv4.gif)
 > blinking text 
-- random characters 
-![preview](https://i.imgur.com/RYxSTDk.gif)
-> spy movies like text reveal
+![preview](https://i.imgur.com/2M2wDUD.gif) 
 # Contribute 
 Report bugs or suggestions  in issues.
--- scripted_text.py
+++ scripted_text.py
@@ -1,12 +1,12 @@
 import obspython as obs
-from itertools import cycle, repeat
+from itertools import cycle
 from functools import partial
-from random import choice, seed
 from time import sleep
 import string
 
-__author__ = 'upgradeQ'
-__version__ = '0.1.0'
+__author__ = "upgradeQ"
+__version__ = "0.1.1"
+
 
 class TextContent:
     def __init__(self, text_string="This is default text", source_name=None):
@@ -40,11 +40,9 @@ class Driver(TextContent):
         self.refresh_rate = 250
         self.duration = 5 * 1000
         self.effect_duration = 3 * 1000
-        self.l = []
-        self.i = 0
 
         self.blinking = cycle([True, False])
-        self.palette = cycle([0xFFBE0B, 0xFB5607, 0xFF006E, 0x8338EC, 0x3A86FF])  # csv
+        self.palette = cycle([0xFFBE0B, 0xFB5607, 0xFF006E, 0x8338EC, 0x3A86FF]) 
 
     def play_sound(self):
         source = obs.obs_get_source_by_name(self.sound_source_name)
@@ -58,17 +56,14 @@ class Driver(TextContent):
 
     def ticker(self, text_effect):
         """ main time primitive """
+        # those effects updated every self.refresh_rate ms
 
         if text_effect == "static":
             self.static_effect()
         if text_effect == "blink":
             self.blink_effect()
         if text_effect == "rainbow":
-            # self.refresh_rate = 50
             self.rainbow_effect()
-        if text_effect == "random_chars":
-            self.l = list(self.text_chars(self.scripted_text))
-            self.random_effect()
 
         self.duration -= self.refresh_rate
         print(self.duration)
@@ -79,7 +74,6 @@ class Driver(TextContent):
             self.preview = True
             self.hide = True
 
-    # those effects updated every self.refresh_rate ms
     def static_effect(self):
         self.update_text(self.scripted_text)
 
@@ -87,92 +81,29 @@ class Driver(TextContent):
         self.update_text(self.scripted_text, color=next(self.palette))
 
     def blink_effect(self):
-        print("inside blink")
         flag = next(self.blinking)
         if flag:
             self.update_text(self.scripted_text)
         else:
             self.update_text("")
 
-    def random_effect(self):
-        try:
-            self.update_text(self.l[self.i])
-            self.i += 1
-        except IndexError:
-            self.refresh_rate = 0
-            self.update_text(self.scripted_text)
-            sleep(1)  # sometimes causes to print additional characters after...
-            # setting all to default
-            self.i = 0
-            self.refresh_rate = 250
-
-    def text_chars(self, scripted_str):
-        """inspired by https://github.com/etienne-napoleone/scrmbl """
-
-        ALL_CHARS = string.digits + string.ascii_letters + string.punctuation
-
-        def gen(scripted_str, iterations):
-            seed()  # set new seed
-            echoed = ""
-            fill = len(scripted_str)
-            for char in scripted_str:
-                for _ in range(iterations):
-                    if char != " ":
-                        ran_char = choice(ALL_CHARS)
-                        yield f"{echoed}{ran_char}{' '*fill}"
-                    else:
-                        yield f"{echoed}{' '*fill}"
-                echoed += char
-            if echoed:  # last char
-                yield f"{echoed}"
-
-        return gen(scripted_str, 5)
-
     def hotkey_hook(self):
         """ trigger hotkey event"""
         self.play_sound()
+        print("effect duration ", self.effect_duration)
         self.duration = self.effect_duration
         print("effect duration ", self.duration)
-        if self.effect == "random_chars":
-            l = len(self.l)
-            static_duration = 1
-            try:
-                t = (self.duration - static_duration) // l
-                self.refresh_rate = t
-            except ZeroDivisionError:  # startup bug
-                self.refresh_rate = 50
         interval = self.refresh_rate
         if self.preview:
             self.ticker = partial(self.ticker, text_effect=self.effect)
             obs.timer_add(self.ticker, interval)
         self.preview = False
 
-    def refresh_text(self):
-        self.play_sound()
-        """ trigger refresh button event"""
-        self.duration = 5000
-        self.refresh_rate = 250
-        print("refresh pressed", repr(self.scripted_text))
-        self.update_text(self.scripted_text)
-
 
 scripted_text_driver = Driver(
     text_string="default string", source_name="default source name"
 )
 
-
-def refresh(prop, props):
-    """ refresh button"""
-    scripted_text_driver.refresh_text()
-    interval = scripted_text_driver.refresh_rate
-    effect = scripted_text_driver.effect
-    print(effect)
-    if scripted_text_driver.preview:
-        scripted_text_driver.ticker = partial(
-            scripted_text_driver.ticker, text_effect=effect
-        )
-        obs.timer_add(scripted_text_driver.ticker, interval)
-    scripted_text_driver.preview = False
 
 
 def script_description():
@@ -188,15 +119,14 @@ def script_update(settings):
     scripted_text_driver.refresh_rate = obs.obs_data_get_int(settings, "refresh_rate")
 
     print("setting duration")
-    ed = scripted_text_driver.effect_duration = obs.obs_data_get_int(settings, "duration")
-    if ed <=0:
-        scripted_text_driver.effect_duration = 1000 # bug refresh rate not applied when not updated
-    
+    scripted_text_driver.effect_duration = obs.obs_data_get_int(
+        settings, "duration"
+    )
     scripted_text_driver.sound_source_name = obs.obs_data_get_string(
         settings, "playsound"
     )
     scripted_text_driver.effect = obs.obs_data_get_string(settings, "text_effect")
-    print('stopping sound')
+    print("stopping sound")
     scripted_text_driver.stop_sound()
 
 
@@ -217,8 +147,8 @@ def script_properties():
     obs.obs_properties_add_text(
         props, "scripted_text", "Scripted text", obs.OBS_TEXT_DEFAULT
     )
-    obs.obs_properties_add_int(props, "refresh_rate", "Refresh rate(ms)", 50, 5000, 1)
-    obs.obs_properties_add_int(props, "duration", "Duration shown(ms)", 5000, 15000, 1)
+    obs.obs_properties_add_int(props, "refresh_rate", "Refresh rate(ms)", 50, 5*1000, 1)
+    obs.obs_properties_add_int(props, "duration", "Duration shown(ms)", 1*1000, 3600*1000, 1)
 
     p = obs.obs_properties_add_list(
         props,
@@ -242,7 +172,7 @@ def script_properties():
         obs.OBS_COMBO_FORMAT_STRING,
     )
 
-    for i in ["rainbow", "static", "random_chars", "blink"]:
+    for i in ["rainbow", "static", "blink"]:
         obs.obs_property_list_add_string(tp, i, i)
 
     sources = obs.obs_enum_sources()
@@ -259,7 +189,8 @@ def script_properties():
 
         obs.source_list_release(sources)
 
-    obs.obs_properties_add_button(props, "button", "Refresh and preview(5s)", refresh)
+    obs.obs_properties_add_button(props, "button1", "preview",lambda *props:scripted_text_driver.hotkey_hook())
+
     return props
 
 
